2a9a58b1ad9e1988228c5b7bc00ed26c2d0bfee5
Obafemi Ogunmokun <obafemi.o@ng.lopworks.com>
2019-07-29 16:27:39 +0100

    Connected to MongoDB

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const path = require('path')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/nodekb')
+
+let db = mongoose.connection
+
+// Check connection
+db.once('open', function(){
+    console.log('Connected to MongoDB')
+})
+
+// check for db errors
+db.on('error', function(err){
+    console.log(err)
+})
 
 // Init App
 const app = express()
+
+// Bring in models
+let Article = require('./models/article')
 
 // Load view engine
 app.set('views', path.join(__dirname, 'views'));
@@ -10,8 +28,24 @@ app.set('view engine', 'pug');
 
 // Home route
 app.get('/', function(req, res){
-    res.render('index', {
-        title:'Articles'
+    Article.find({}, function(err, articles){
+        if(err){
+            console.log(err)
+        } else {
+            res.render('index', {
+                title:'Articles',
+                articles: articles
+            })
+        }
+            
+    })
+    
+})
+
+// Add route
+app.get('/articles/add', function (req, res) {
+    res.render('add_article', {
+        title: 'Add Article'
     })
 })
 
