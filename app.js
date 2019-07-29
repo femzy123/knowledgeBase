e1b50f6097d1a7942fc34158f9c080a43286e51a
Obafemi Ogunmokun <obafemi.o@ng.lopworks.com>
2019-07-29 07:18:20 +0100

    Added pug template engine

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const path = require('path')
 
+// Init App
 const app = express()
 
+// Load view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Home route
 app.get('/', function(req, res){
-    res.send('Hello World')
+    res.render('index', {
+        title:'Articles'
+    })
 })
 
+// Start server
 app.listen(3000, function(){
     console.log('Server started on port 3000...')
 })
