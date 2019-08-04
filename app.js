844254f7a1ee74bdca4df5fb4d2ee235b6ddc0ca
Obafemi Ogunmokun <femzy123@yahoo.com>
2019-08-04 20:08:47 +0100

    Create new articles using body-parser

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path')
 const mongoose = require('mongoose')
+const bodyParser = require("body-parser");
 
 mongoose.connect('mongodb://localhost/nodekb')
 
@@ -26,6 +27,14 @@ let Article = require('./models/article')
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+
+// Body Parser Middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 // Home route
 app.get('/', function(req, res){
     Article.find({}, function(err, articles){
@@ -46,6 +55,23 @@ app.get('/', function(req, res){
 app.get('/articles/add', function (req, res) {
     res.render('add_article', {
         title: 'Add Article'
+    })
+})
+
+// Add Submit POST Route
+app.post('/articles/add', function (req, res) {
+    let article = new Article()
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    article.save(function(err) {
+        if(err){
+            console.log(err)
+            return;
+        } else {
+            res.redirect('/');
+        }
     })
 })
 
